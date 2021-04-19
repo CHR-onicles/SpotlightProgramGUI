79ad88e8020f9c7247e438ebd6b3421e032ed0e5
CHR-onicles <tpandivine48@gmail.com>
2021-04-19 13:27:35 +0000

    minor additions

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ build_options = {'zip_include_packages': ['PyQt5.QtWidgets', 'PyQt5.QtGui', 'PyQ
                               ]
                  }
 
-base = None  # todo: (not a todo) Uncomment for testing and debugging
-# base = 'Win32GUI' if sys.platform == 'win32' else None
+# base = None  # todo: (not a todo) Uncomment for testing and debugging
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
     Executable('main.py', base=base, icon='img/cat.ico', target_name='Spotty App')
